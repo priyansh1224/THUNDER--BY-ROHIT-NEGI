7269867683863fd3040341319e7b8d85a8960057
priyansh1224 <priyanshuoli30@gmail.com>
2026-06-17 11:38:58 +0530

    day01 complete

---
+++ javaScript/Day01/index.js
@@ -0,0 +1,9 @@
+/**
+HTML is used to create static web pages.
+JavaScript makes web pages dynamic and interactive.
+JavaScript was created by Brendan Eich in 1995 at Netscape Communications in about 10 days.
+Many companies use web applications to reduce app-store commission fees.
+WebAssembly (WASM) enables languages such as C, C++, and Rust to run efficiently in browsers.
+JavaScript code can interact with programs written in other languages through various execution engines and APIs.
+Project Idea: Build a simple compiler/interpreter that reads code and executes it inside a custom terminal environment.
+ */
