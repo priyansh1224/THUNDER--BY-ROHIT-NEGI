ccbb887e9f79c0e292e2d5d34ae9b5a5e5348743
priyansh1224 <priyanshuoli30@gmail.com>
2026-06-21 17:13:45 +0530

    #

---
+++ javaScript/Day02/first.js
@@ -0,0 +1,127 @@
+// console.log("Hello world");
+
+// Burder other language: Leave that
+
+// let a = 10.6;
+// a = 30;
+
+// // string type
+
+// let firstString = "Rohit Negi";
+
+// console.log(a);
+
+// const a = 10.9;
+// a = 7;
+
+// console.log(a);
+
+// let a = 30
+// let b = a;
+
+// b = 70;
+
+// console.log(a,b);
+
+// let a = 10;
+// a = 20;
+
+// console.log(a);
+
+// primitive data type and non primitive data type
+
+// primitive data: number, string, boolean, undefined, null, symbol, bigint
+// immutable
+
+
+// number
+// let firstNumber = 20;
+// let secondNumber = 20.7;
+
+// console.log(firstNumber,secondNumber);
+
+// // string
+
+// let firstString = "Rohit Negi";
+// let secondString = 'Mohan Bhaiya';
+// let thirdString = `Rohit ${secondNumber} is a Good Boy ${firstNumber} `;
+
+// console.log(firstString,secondString,thirdString);
+
+// Boolean
+
+// let firstBoolean = true;
+// let secondBoolean = false;
+
+// console.log(firstBoolean,secondBoolean);
+
+
+// undefined
+
+// let a;
+// console.log(a);
+
+
+// Null
+
+// let b = null;
+// console.log(b);
+
+
+// bigint
+
+// let a = 27343285947319574913n;
+// console.log(a);
+
+// symbolId
+
+// let a = Symbol("Rohit");
+// let b = Symbol("Rohit");
+// console.log(b==a);
+
+// non primitive: object, array, function
+
+
+// let arr = [10,20,30,"Rohit",9.3, true, 90];
+// console.log(arr);
+
+
+// object: Sabse jaada important hai
+// let person = {
+//     name:"Rohit",
+//     age: 30,
+//     city: "dwarka"
+// }
+
+// console.log(person);
+
+// let a = function greet(){
+//     console.log("Hello akshat");
+// }
+
+
+// let a = function(){
+//    console.log("hi");
+//    return 10;
+// }
+
+// // console.log(a);
+// console.log(a);
+
+
+// Homework
+
+// let a = "Rohit";
+
+// console.log(typeof(a));
+
+// Primitive data type
+// Non primitive data
+
+let a = {
+    name:"Rohit",
+    age:20
+}
+
+console.log(typeof(a));
+
---
+++ notes.md
@@ -0,0 +1,23 @@
+Lecture01 Notes: https://app.notion.com/p/Lecture01-Introduction-to-Javascript-37243ac5cab9802293fff4573c26a6f4?source=copy_link
+
+Lecture02 Notes: https://app.notion.com/p/Lecture-02-Data-types-in-JS-37343ac5cab980f8b24ee3cf1ea0c8fa?source=copy_link
+
+Lecture03 Notes: https://app.notion.com/p/Lecture03-Operator-and-Data-type-in-JS-37543ac5cab9805bb338dc7e6c3ab515?source=copy_link
+
+Lecture04: https://app.notion.com/p/Lecture04-Loop-Number-math-and-String-37643ac5cab9802ba80ffca6c7e961d6?source=copy_link
+
+Lecture05: https://app.notion.com/p/Lecture05-37743ac5cab980fc90afeec0d60a0fda?source=copy_link
+
+Lecture06: https://app.notion.com/p/Lecture06-Array-and-Objects-in-Javascript-37943ac5cab9807f801cc8c83755decc?source=copy_link
+
+Lecture07: https://app.notion.com/p/Lecture-07-Objects-and-Date-37b43ac5cab980cfa8d3db0bf87411b2?source=copy_link
+
+Lecture08: https://app.notion.com/p/Lecture08-Date-and-Functions-in-JS-37c43ac5cab98043bcfafdc2a70c7a3a?source=copy_link
+
+Lecture09: https://app.notion.com/p/Lecture09-Callback-forEach-map-filter-reduce-37d43ac5cab980e0a44ef39a89b81143?source=copy_link
+
+Lecture10: https://app.notion.com/p/Lecture10-Introduction-To-DOM-38043ac5cab980adbbdeffd5e8dc6ae8?source=copy_link
+
+Lecture11: https://app.notion.com/p/Lecture11-CRUD-and-Event-in-DOM-38143ac5cab980d48176fda6b086cfef?source=copy_link
+
+Lecture12: https://app.notion.com/p/Lecture12-Even-and-Project-in-Javascript-38343ac5cab980aab918f7f4dc5c2fff?source=copy_link
