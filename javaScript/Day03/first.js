9d907f7f80acb75c741ffbb577e82a7439118357
priyansh1224 <priyanshuoli30@gmail.com>
2026-06-25 15:50:00 +0530

    Day03 completed

---
+++ javaScript/Day03/first.js
@@ -0,0 +1,72 @@
+// Primitive data type
+
+// let firstNumber = 10;
+// let secondNumber = firstNumber;
+// // let thirdNumber = firstNumber;
+
+// secondNumber = 20;
+
+// console.log(firstNumber,secondNumber);
+
+
+// let obj1 = {
+//     name:"Rohit",
+//     age:10
+// }
+
+// let obj2 = obj1;
+
+// obj2.name = "Mohan";
+// console.log(obj1);
+
+
+// let a = "Rohit";
+// let b = "Rohit";
+
+
+
+// console.log(a==b);
+
+
+// Non Primitive data compare on the basis of reference
+
+// let obj1 = {
+//     name: "Rohit",
+//     age: 20
+// }
+
+
+// let obj2 = {
+//     name: "Rohit",
+//     age: 20
+// }
+
+// obj2.name = "Mohan";
+
+
+// console.log(obj1)
+
+// let a = 20;
+// a = 30;
+// console.log(a);
+
+// const a = 10;
+// const b = a;
+
+// b = 30;
+
+// console.log(a==b);
+
+const a = {
+    name: "Rohit",
+    age: 20
+}
+
+
+// a.age = 10;
+
+a = {
+    age: 20,
+}
+
+console.log(a);
---
+++ javaScript/Day03/second.js
@@ -0,0 +1,142 @@
+// let const
+
+// var: Old method
+
+// console.log(a);
+// var a = 10;
+
+// console.log(a);
+// let a = 10;
+
+
+// if(true){
+//     let a = 10;
+// }
+
+// console.log(a);
+
+// let a = 10+20;
+
+
+
+// operator in javascript
+// arithmetic operation
+
+// console.log(3+4);
+// console.log(3-4);
+// console.log(3*4);
+// console.log(10/4);
+// console.log(10%4);
+// console.log(10**4);
+
+
+let a = 10;
+let b = 20;
+
+// a = a+b;
+// a+=b;
+// a/=b;
+// console.log(a);
+
+// post increment
+// a++;
+
+// pre incremenet
+// ++a;
+// console.log(a++);
+// console.log(a);
+
+// pehle aap a ke original value le lijiye, uske baad a increase hoga 1 se
+
+
+// preincrement: Pehle mein apni value ko 1 se increase karunga, uske baad aapko value dunga
+// console.log(++a);
+
+// a = 10
+// let k = ++a;
+// console.log(k);
+// console.log(a);
+
+
+// comparison operator
+
+// console.log(10>5);
+// console.log(10>=5)
+// console.log(10<=5)
+// console.log(10<5)
+
+// Gadhe coder ye hurkat karte hai, isko seekhte hai, aur apna time poora waste karte hai
+
+// console.log(10==10);
+// "10" == 10
+// 10 == 10
+// console.log("10a"==10);
+
+
+// HTML ke andar kitne bacho ne form banaye
+
+// name: "Rohit"
+// age: 10
+// city: "kotdwar"
+
+// let age = Number("10");
+// console.log(age);
+
+
+// calculator:
+// first = 10, second = 20
+
+// console.log(Number("10")+Number("20"));
+
+// console.log(String(30)+'7');
+
+
+// console.log(null==undefined);
+
+//  JS: Null is loosely equal to undefined and not to anyone: ==
+
+// console.log(null==undefined);
+// console.log(null==0);
+// console.log(null==1);
+// console.log(null==false);
+
+// comparison operator aayenge: >,>=,<,<=
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null>0);
+// console.log(null<0);
+
+// let ab = 10*2+5*64+78/2;
+// console.log(ab);
+
+// let ab = ((((10*2)+5)*64)+(78/2))
+// // ab = 25*64 + 39
+// console.log(ab);
+
+
+// console.log("10"===10);
+// ===, type ko check, fr apki value ko compare karta hai
+
+
+// console.log(0==false);
+
+
+// operator important
+
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&true);
+// console.log(false&&false);
+
+// console.log(true&&"Rohit");
+
+// // console.log(true&&"Rohit");
+// console.log(false&&"Rohit");
+
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||true);
+// console.log(false||false);
+
+
+console.log(false || false);
