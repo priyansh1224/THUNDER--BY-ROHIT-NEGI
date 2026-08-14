e84cc3e431f27df75a8216eba845cce17a36d96f
priyansh1224 <priyanshuoli30@gmail.com>
2026-08-14 08:14:34 +0530

    Day02 of ChatGpt is completed successfully

---
+++ ChatGpt/Day02/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from 'dotenv'
+import connectDB from "./config/database.js";
+import userRouter from "./routes/userRouter.js"
+import messageRouter from "./routes/messageRouter.js"
+import cookieParser from "cookie-parser"
+dotenv.config();
+
+const app=express();
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/user",userRouter)
+app.use("/msg",messageRouter)
+
+const startServer=async()=>{
+    try {
+        await connectDB();
+
+        app.listen(process.env.PORT,()=>{
+        console.log(`server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
---
+++ ChatGpt/Day02/middlewares/authUserMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+import User from "../model/userSchema";
+
+const authUserMiddleware=async(req,res,next)=>{
+    try {
+        const {token}=req.cookies;
+
+        const payload=jwt.verify(token,process.env.JWT_SECRET);
+
+        const existingUser=await User.findById(payload.id);
+        if(!existingUser){
+            return res.status(401).json({message:"User Doesn't Exists"})
+        }
+
+        req.user=existingUser;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
+}
+export default authUserMiddleware;
